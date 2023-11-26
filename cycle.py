f7096ab015dd03b2e02d69900a35924dc5b27d40
Leonard <leonardgentwintang@gmail.com>
2023-11-26 18:42:39 -0500

    StarCoder has some really bad formatting. To be figured out and/or just use differenet model

--- cycle.py
+++ cycle.py
@@ -4,10 +4,11 @@ from datasets import load_dataset
 from pprint import pprint
 from torch.utils.data import DataLoader
 from transformers import BitsAndBytesConfig, DataCollatorForLanguageModeling
-from utils import setup_model_tokenizer
+from utils import construct_stopping_criteria, setup_model_tokenizer
 
 # Invariants
 BATCH_SIZE = 32
+NEW_TOKENS = 128
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_use_double_quant=True,
@@ -19,8 +20,6 @@ bnb_config = BitsAndBytesConfig(
 # Each problem includes a function signature, docstring, body, and several unit tests, with an average of 7.7 tests per problem
 # features: ['task_id', 'prompt', 'canonical_solution', 'test', 'entry_point']
 dataset = load_dataset("openai_humaneval")
-print("Dataset:", dataset)
-print("Sample problem:", dataset["test"][0])  # Print the first problem as a sample
 
 
 def extract_docstring(text):
@@ -28,14 +27,16 @@ def extract_docstring(text):
         r"'''(.*?)'''|\"\"\"(.*?)\"\"\"", text, re.DOTALL
     )
     extracted_strings = [
-        f'"""{match[0]}"""' if match[0] else f'"""{match[1]}"""' for match in triple_quoted_strings
+        f'"""{match[0]}"""' if match[0] else f'"""{match[1]}"""'
+        for match in triple_quoted_strings
     ]
-    return extracted_strings
+    return extracted_strings[0]
 
 
 def preprocess_prompt(example):
     example["docstring"] = extract_docstring(example["prompt"])
     return example
+
 
 dataset = dataset.map(preprocess_prompt)
 
@@ -49,20 +50,74 @@ gen_tokenizer, gen_model = setup_model_tokenizer(
 # Ranker setup
 rank_checkpoint = "kdf/python-docstring-generation"
 rank_device = "cuda"
-rank_tokenizer, rank_model = setup_model_tokenizer(rank_checkpoint).to(rank_device)
+rank_tokenizer, rank_model = setup_model_tokenizer(
+    rank_checkpoint, bit_4=True, device=gen_device, bnb_config=bnb_config
+)
 
-# # Main loop
-# data_collator = DataCollatorForLanguageModeling(tokenizer=gen_tokenizer, mlm=False)
-# train_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=data_collator)
-
+dataloader = DataLoader(dataset["test"], batch_size=1, shuffle=False)
 # HACK: Dataset is so small that let's just loop through it one-by-one for now LOL
-for i, data in enumerate(dataset):
-    prompt = data['prompt']
-    gen_inputs = gen_tokenizer.encode(prompt, return_tensors="pt").to(gen_device)
-    gen_outputs = gen_model.generate(**gen_inputs)
-    print('Gen Outputs', gen_outputs)
-    
-    rank_inputs = rank_tokenizer.encode(gen_outputs, return_tensors="pt").to(rank_device)
+# Fuck the Stanford people fr how dare they scoop my idea
+for i, data in enumerate(dataloader):
+    # print("data", data)
+    og_prompt = data["prompt"][0]
+    # StarCoder magic
+    magic_prefix = (
+        "<filename>solutions/solution_1.py\n"
+        "# Here is the correct implementation of the code exercise\n"
+    )
+    prompt = magic_prefix + og_prompt
+    print("##### Prompt #####")
+    print(prompt)
+    docstring = data["docstring"][0]
+    # print("##### Docstring #####", docstring)
+    # Forward-generate program.
+    # Definitely want this to be sampled
+    gen_inputs = gen_tokenizer(prompt, return_tensors="pt").to(gen_device)
+    # print("gen_inputs shape", gen_inputs['input_ids'].shape)
+    # TODO: think about temperature and top-p
+    gen_outputs_dict = gen_model.generate(
+        **gen_inputs,
+        pad_token_id=gen_tokenizer.eos_token_id,
+        max_new_tokens=NEW_TOKENS,
+        return_dict_in_generate=True,
+        stopping_criteria=construct_stopping_criteria(
+            "code", [], gen_tokenizer, gen_device
+        ),
+    )
+
+    # print('gen_inputs["input_ids"].shape[1]', gen_inputs["input_ids"].shape[1])
+    # print('gen_outputs_dict.sequences', gen_outputs_dict.sequences)
+    gen_outputs = gen_outputs_dict.sequences[
+        :, gen_inputs["input_ids"].shape[1] :
+    ].squeeze()
+
+    # print("Gen Outputs Shape", gen_outputs.shape)
+
+    # Attempt to recover doctring
+    # TODO: think about better prompting or (e.g. some form of estimate P(y|x))
+    # Not sure if we also want this to be sampled
+    rank_inputs = gen_tokenizer.decode(gen_outputs, skip_special_tokens=True)
+    # print("OG rank inputs", rank_inputs)
+    # Based on HF page
+    full_code = og_prompt + rank_inputs
+
+    # TOOD: run full_code on the test set
+
+    print('##### FULL CODE #####')
+    print(full_code)
+
+    rank_inputs += f"\n\n#docstring"
+    print("##### Formatted rank inputs #####")
+    print(rank_inputs)
+    rank_inputs = rank_tokenizer(rank_inputs, return_tensors="pt").to(rank_device)
+    # TODO: better stopping criteria; also figureo out what to do with empty string
+    rank_outputs = rank_model.generate(
+        **rank_inputs,
+        pad_token_id=rank_tokenizer.eos_token_id,
+        max_new_tokens=NEW_TOKENS,
+    )[0]
+    rank_outputs = rank_tokenizer.decode(rank_outputs, skip_special_tokens=True)
+    print("##### Rank outputs #####", rank_outputs)
 
 # doc_max_length = 128
 
---
+++ utils.py
@@ -0,0 +1,82 @@
+import torch
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    StoppingCriteria,
+    StoppingCriteriaList,
+)
+
+
+# TODO: figure out how to stop only after second time witness of stop_token_ids
+class CodingStop(StoppingCriteria):
+    def __init__(self, stop_words, tokenizer, device) -> None:
+        StoppingCriteria.__init__(self)
+        stop_token_ids = [
+            tokenizer(x, return_tensors="pt")["input_ids"].squeeze() for x in stop_words
+        ]
+        stop_token_ids = [torch.LongTensor(x).to(device) for x in stop_token_ids]
+        self.stop_token_ids = stop_token_ids
+
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+        for stop_ids in self.stop_token_ids:
+            if torch.eq(input_ids[0][-len(stop_ids) :], stop_ids).all():
+                return True
+        return False
+
+# Potential stopping criteria based on StarCoder observations
+# if __name__ == "__main__":
+
+# Also need to check tab sizing??
+def construct_stopping_criteria(type, stop_words, tokenizer, device):
+    if type == 'code':
+        return StoppingCriteriaList([CodingStop(stop_words, tokenizer, device)])
+    elif type == 'doc':
+        pass
+
+
+def setup_tokenizer(path):
+    tokenizer = AutoTokenizer.from_pretrained(
+        path,
+        trust_remote_code=True,
+    )
+    if "starcoder" in path:
+        pass
+
+    return tokenizer
+
+
+def setup_model_tokenizer(
+    path,
+    device=None,
+    bit_4=False,
+    bit_8=False,
+    max_memory=None,
+    bnb_config=None,
+):
+    tokenizer = setup_tokenizer(path)
+    if torch.cuda.device_count() > 1:
+        model = AutoModelForCausalLM.from_pretrained(
+            path,
+            trust_remote_code=True,
+            device_map="auto",
+            load_in_4bit=bit_4,
+            load_in_8bit=bit_8,
+            max_memory=max_memory,
+            quantization_config=bnb_config,
+        ).eval()
+    else:
+        if not bit_4 and not bit_8:
+            model = (
+                AutoModelForCausalLM.from_pretrained(path, trust_remote_code=True)
+                .to(device)
+                .eval()
+            )
+        else:
+            model = AutoModelForCausalLM.from_pretrained(
+                path,
+                trust_remote_code=True,
+                load_in_4bit=bit_4,
+                load_in_8bit=bit_8,
+                quantization_config=bnb_config,
+            ).eval()
+    return tokenizer, model
