a42c9aad4e70ecf4dc33b61e7b95b304cc4670b4
Leonard <leonardgentwintang@gmail.com>
2023-12-09 23:20:03 -0500

    Parallelized exps

--- experiments_driver.sh
+++ experiments_driver.sh
@@ -16,7 +16,7 @@ conda activate redteam
 # This is a simplistic check. Replace 2000 with the approximate memory requirement of your task in MiB.
 check_gpu_memory() {
     local gpu_id=$1
-    local required_memory=40000
+    local required_memory=65000
     local free_memory=$(nvidia-smi --query-gpu=memory.free --format=csv,noheader,nounits -i $gpu_id)
 
     if [ "$free_memory" -ge "$required_memory" ]; then
@@ -37,25 +37,38 @@ run_on_gpu() {
     # Replace spaces with underscores and commas with dashes for filename
     local filename=$(echo "$params" | sed 's/ /_/g' | sed 's/,/-/g')
 
-    # TODO: figure out safe way of doing parallel
+    # Parallel execution
     CUDA_VISIBLE_DEVICES=$gpu_id python cycle.py $params > "results_${filename}.txt" &
     # CUDA_VISIBLE_DEVICES=$gpu_id python cycle.py $params > "results_${filename}.txt"
 }
+
+attempt_run_on_gpu() {
+    local params=$1
+    local gpu_found=false
+
+    while [ "$gpu_found" = false ]; do
+        for (( gpu_id=0; gpu_id<num_gpus; gpu_id++ )); do
+            if check_gpu_memory $gpu_id; then
+                run_on_gpu $gpu_id "$params"
+                gpu_found=true
+                break 2 # Exit both the for and while loops
+            fi
+        done
+
+        echo "Waiting for a GPU to become available..."
+        sleep 100 # Sleep for 10 seconds before retrying
+    done
+}
+
 
 # Get the number of GPUs
 num_gpus=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 
 # Generate hyperparameter combinations and iterate over them
 python generate_hps.py | while IFS=, read -r selectcrit fewshot gentemp repeat
-do
-    for (( gpu_id=0; gpu_id<num_gpus; gpu_id++ ))
-    do
-        if check_gpu_memory $gpu_id; then
-            run_on_gpu $gpu_id "--select-crit $selectcrit --few-shot $fewshot --gen-temp $gentemp --repeat $repeat"
-            break # Break the loop if we found a GPU with enough memory
-        fi
-    done
-    sleep 10
+doe
+    attempt_run_on_gpu "--select-crit $selectcrit --few-shot $fewshot --gen-temp $gentemp --repeat $repeat"
+    sleep 20
 done
 
 wait
